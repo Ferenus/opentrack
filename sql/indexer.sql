d80a0ab32719b3d7ea26be86c1bc50f6cb14b39f
Daniel Stradowski <daniel.stradowski@gmail.com>
2021-11-12 03:32:09 +0100

    indexer

--- sql/indexer.sql
+++ sql/indexer.sql
@@ -1,5 +1,11 @@
-SELECT container_id, port.name AS "port_name", terminal.name AS "terminal_name", terminal.firms_code, vessel.name AS "vessel_name", vessel.imo
-FROM container
-       LEFT JOIN vessel ON vessel.id = container.vessel_id
-       LEFT JOIN terminal ON terminal.id = vessel.terminal_id
-       LEFT JOIN port ON port.id = terminal.port_id;
+SELECT to_json(a) FROM (SELECT container_id,
+                               port.name     AS "port_name",
+                               terminal.name AS "terminal_name",
+                               terminal.firms_code,
+                               vessel.name   AS "vessel_name",
+                               vessel.imo
+                        FROM container
+                               LEFT JOIN vessel ON vessel.id = container.vessel_id
+                               LEFT JOIN terminal ON terminal.id = vessel.terminal_id
+                               LEFT JOIN port ON port.id = terminal.port_id
+) AS a
